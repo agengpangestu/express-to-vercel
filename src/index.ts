d225c127da7bffccbde02390d03d6866d6f47ae2
Ageng Pangestu <agengp617@gmail.com>
2024-09-11 13:18:08 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("WORK!");
+});
+
+app.get("/ping", (_req: Request, res: Response) => {
+  res.send("Pong 🏓!");
+});
+
+app.listen(port, () => console.log(`server running at ${port}`));
